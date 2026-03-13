a4287ebc3cd5afad55bd49529de266d79cbd9a53
rastor65 <rdamianquintero@uniguajira.edu.co>
2026-03-13 10:29:10 -0500

    .

--- FrontendRevistas/src/environments/environment.prod.ts
+++ FrontendRevistas/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   
-  // API_URI : 'http://192.168.8.32:10930'
-  API_URI : 'http://localhost:8000'
+  API_URI : 'http://192.168.8.32:10930'
+  //API_URI : 'http://localhost:8000'
   
 };
